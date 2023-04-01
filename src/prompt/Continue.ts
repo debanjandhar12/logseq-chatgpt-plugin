e7047e1d1fab45ad72e3f763ceb67da6ea02ed7c
debanjandhar12 <49021233+debanjandhar12@users.noreply.github.com>
2023-04-01 16:19:56 +0530

    enhance: add more prefix messages

--- src/prompt/Continue.ts
+++ src/prompt/Continue.ts
@@ -6,6 +6,14 @@ export class Continue {
             {
                 name: 'Continue',
                 required_input: 'block(s)',
+                getPromptPrefixMessages: () => [
+                    {'role': 'user', 'content': `I want you to act as a Autocomplete tool. You take the input complete it factually. DO NOT reply the context of the question of the user input. Sample of the conversation will show below:
+                    user: Continue: *text*
+                    you: *text with completion*
+                    user: add *new info*
+                    you: *text with completion and new info*
+                    `.trim()}
+                ],
                 getPrompt: () => `Continue:`,
                 group: 'continue'
             }
--- src/prompt/Fix.ts
+++ src/prompt/Fix.ts
@@ -8,7 +8,7 @@ export class Fix {
                 required_input: 'block(s)',
                 getPromptPrefixMessages: () => [
                     {'role': 'user', 'content': `
-                    You correct and make the sentence more fluent only when asked by user. You take all the user input and auto correct it. Just reply to user input with correct grammar, DO NOT reply the context of the question of the user input. If the user input is grammatically correct and fluent, just inform user about the same. Sample of the conversation will show below:
+                    I want you to correct and make the sentence more fluent when asked by me. You take the input and auto correct it. Just reply to user input with correct grammar, DO NOT reply the context of the question of the user input. If the user input is grammatically correct and fluent, just inform user about the same. Sample of the conversation will show below:
                     user: Fix Grammar: *incorrect text*
                     you: *correct text*
                     user: Fix Grammar: *Grammatically correct text*
@@ -21,6 +21,15 @@ export class Fix {
             {
                 name: 'Fix Spelling',
                 required_input: 'block(s)',
+                getPromptPrefixMessages: () => [
+                    {'role': 'user', 'content': `
+                    I want you to correct spelling when asked by me. You take the input and auto correct it. Just reply to user input with correct spelling, DO NOT reply the context of the question of the user input. If the user input is correct, just inform user about the same. Sample of the conversation will show below:
+                    user: Fix Spelling: *misspelled text*
+                    you: *text with correct spelling*
+                    user: Fix Spelling: *text with correct spelling*
+                    you: *inform user that the text is correct*
+                    `.trim()}
+                ],
                 getPrompt: () => `Fix Spelling:`,
                 group: 'fix'
             }
