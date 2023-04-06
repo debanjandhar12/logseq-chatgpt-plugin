8e5e6b1d5975dc3db4e8aa3e6f3c2b1a9bdddf44
debanjandhar12 <49021233+debanjandhar12@users.noreply.github.com>
2023-04-06 13:12:39 +0530

    enhance(prompt): continue

--- src/prompt/Continue.ts
+++ src/prompt/Continue.ts
@@ -7,13 +7,11 @@ export class Continue {
                 name: 'Continue',
                 required_input: 'block(s)',
                 getPromptPrefixMessages: () => [
-                    {'role': 'user', 'content': `I want you to act as a Autocomplete tool. You take the input complete it factually. DO NOT reply the context of the question of the user input. Sample of the conversation is shown below:
+                    {'role': 'user', 'content': `I want you to act as a Autocomplete tool. You take the input and complete it factually. DO NOT reply the context in the next reply only. Sample of the conversation is shown below:
                     user: Continue: *text*
                     you: *rest of text*
                     user: *add new info*
-                    you: *complete text*
-                    user: *question related to text*
-                    you: *complete text*
+                    you: *repeat entire text with added info*
                     `.replaceAll('    ', '').trim()}
                 ],
                 getPrompt: () => `Continue:`,
